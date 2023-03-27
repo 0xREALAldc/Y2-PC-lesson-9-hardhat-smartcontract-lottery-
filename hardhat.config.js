10ed7213291b20fa3dd25d26ddd66ad98bd193f6
0xREALaldc <andredonat@hotmail.com>
2023-03-27 19:28:36 -0300

    Add: configurations to use in the deploy of the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,35 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://sepolia.etherscan.io/"
+const PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || "0xKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat", 
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      chainId: 11155111,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    }
+  },
   solidity: "0.8.7",  
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
